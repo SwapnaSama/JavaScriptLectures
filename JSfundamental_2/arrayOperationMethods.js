d8d34450f5158abc503e917c2abf724c5709c5df
Swapna Sama <swapnasama9@gmail.com>
2021-07-08 00:24:50 -0400

    deleted prettier and modified arrayOperations

--- JSfundamental_2/arrayOperationMethods.js
+++ JSfundamental_2/arrayOperationMethods.js
@@ -1,31 +1,30 @@
-const friends = ['raki', 'janu', 'raji'];
-friends.push('sama'); // push means adding an element at the end of the array list
+const friends = ["raki", "janu", "raji"];
+friends.push("sama"); // push means adding an element at the end of the array list
 console.log(friends);
 console.log(friends.newLength);
 
-friends.unshift('swarupa'); // unshift means adding element at the beginning of the array list
+friends.unshift("swarupa"); // unshift means adding element at the beginning of the array list
 console.log(friends);
 
 //Removing elements
-friends.pop(); // last element 
+friends.pop(); // last element
 const popped = friends.pop();
-console.log(popped); // removed last element 
+console.log(popped); // removed last element
 console.log(friends);
 
 friends.shift(); //first
 console.log(friends);
 
-console.log(friends.indexOf('janu')); // indexOf is gives the value of element in array list like 1 'janu'
-console.log(friends.indexOf('king')); // indexOf is here is -1 'king'
+console.log(friends.indexOf("janu")); // indexOf is gives the value of element in array list like 1 'janu'
+console.log(friends.indexOf("king")); // indexOf is here is -1 'king'
 
 friends.push(23);
 
-console.log(friends.includes('raki')); //includes checks whether u have the element in arraly list if it is gives true value
-console.log(friends.includes('snailbaby')); // includes checks snailbaby is not present in the array list so gives false
+console.log(friends.includes("raki")); //includes checks whether u have the element in arraly list if it is gives true value
+console.log(friends.includes("snailbaby")); // includes checks snailbaby is not present in the array list so gives false
 console.log(friends.includes(23));
 
 // includes can be used for conditional statements
-if (friends.includes('raki')){
-    console.log('you have a friend called raki');
+if (friends.includes("raki")) {
+  console.log("you have a friend called raki");
 }
-
