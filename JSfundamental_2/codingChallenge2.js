390524e4146373e34a7d14a61ad43b688157feee
Swapna Sama <swapnasama9@gmail.com>
2021-07-11 15:09:33 -0400

    coding Challenge2

--- JSfundamental_2/codingChallenge2.js
+++ JSfundamental_2/codingChallenge2.js
@@ -1,2 +1,7 @@
-// I am about kick start this coding challenge
-console.log("This is my coding challenge");
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [25, 155, 40];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(bills, tips);
