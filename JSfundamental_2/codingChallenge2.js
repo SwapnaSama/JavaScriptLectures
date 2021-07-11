5df50b84f41ee789819098d8f8ddf2857831852f
Swapna Sama <swapnasama9@gmail.com>
2021-07-11 15:29:25 -0400

    codingChallenge2.js

--- JSfundamental_2/codingChallenge2.js
+++ JSfundamental_2/codingChallenge2.js
@@ -1,7 +1,10 @@
 const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
+console.log(calcTip());
 
 const bills = [25, 155, 40];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-console.log(bills, tips);
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
